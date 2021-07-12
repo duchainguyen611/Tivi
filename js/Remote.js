57754299b37a0d7e2d5d91d017e2c06247eaccf4
duchainguyen611 <duchaiprovip01@gmail.com>
2021-07-12 19:44:11 +0700

    Tivi

---
+++ js/Remote.js
@@ -0,0 +1,61 @@
+class Remote {
+    constructor(){
+        this.code = 
+        [[1,2,3],
+        [4,5,6],
+        [7,8,9]];
+    }
+
+    connectTivi(tivi){
+        if(tivi.status){
+            this.tivi = tivi;
+        }else{
+            for(let i=1;i<=9;i++){
+                document.getElementById(i).style.disabled = true;
+            }
+        }
+    }
+
+    display(){
+        let html = '<table>';
+        html += '<tr>'
+        html += '<td>';
+        html += '<button onclick="turnOffTivi()">Off'
+        html += '</td>';
+        html += '<td>'
+        html += '</td>'
+        html += '<td>';
+        html += '<button onclick="turnOnTivi()">On';
+        html += '</button>'
+        html += '</td>';
+
+        this.code.forEach((element, i) => {
+            html += '<tr>';
+            for(const value of element){
+                html+= '<td>'
+                html+= '<button id='+value+' onclick ="remote.changeChanel('+(value-1)+')">';
+                html+= value;
+                html+= '</button>';
+                html+= '</td>';
+            }
+            html += '</tr>';
+        });
+        html += '<tr>';
+        html += '<td><button onclick="upVolumn()">▲</button></td>';
+        html += '<td></td>';
+        html += '<td><button>►</button></td>';
+        html += '</tr>';
+        html += '<tr>';
+        html += '<td><button onclick="downVolumn()">▼</button></td>';
+        html += '<td></td>';
+        html += '<td><button>◄</button></td>';
+        html += '</tr>';
+        html += '</table>';
+        document.getElementById('remote').innerHTML = html;
+    }
+
+    changeChanel(key){
+        this.tivi.setChanel(key);
+        this.tivi.setContent(key);
+    }
+}
---
+++ js/Tivi.js
@@ -0,0 +1,80 @@
+class Tivi {
+    constructor(currentChanel, currentVolumn,currentContent){
+        this.currentChanel = currentChanel;
+        this.currentVolumn = currentVolumn;
+        this.currentContent = currentContent;
+        this.status = false;
+        this.chanel = 
+        ['VTV1','VTV2','VTV3','CN','HBO','Disney','VTV5','Bibi','VTV6'];
+        this.picture = 
+        ['Chanel/VTV1.png','Chanel/VTV2.png','Chanel/VTV3.png','Chanel/CN.png','Chanel/HBO.png','Chanel/Disney.png','Chanel/VTV5.png','Chanel/Bibi.png','Chanel/VTV6.png'];
+    }
+
+    setChanel(code){
+        this.currentChanel = this.chanel[code]; 
+        this.displayLGD();
+    }
+
+    setContent(code){
+        this.currentContent = this.picture[code]; 
+        this.displayLGD();
+    }
+
+    getCurrentChanel(){
+        return this.currentChanel;
+    }
+
+    getCurrentVolumn(){
+        return this.currentVolumn;
+    }
+
+    getCurrentContent(){
+        return this.currentContent;
+    }
+
+    displayLGD(){
+        document.getElementById('currentChanel').innerHTML = this.getCurrentChanel();
+        document.getElementById('currentVolumn').innerHTML = this.getCurrentVolumn();
+        document.getElementById('currentImage').style.backgroundImage = 'url('+this.getCurrentContent()+')';
+    }
+
+    display(){
+        let html = '<div id="content-tivi">';
+        html+= '<div id="currentImage"></div>';
+        html+= '<div id="currentChanel">'
+        html+= this.currentChanel;
+        html+= '</div>'
+        html+= '<br><br><br><br><br>'
+        html+= '<div id="currentVolumn">'
+        html+= this.currentVolumn;
+        html+= '</div>'
+        html+= '</div>';
+        document.getElementById('tivi').innerHTML = html;
+    }
+
+    turnOn(){
+        this.status = true;
+        document.getElementById('content-tivi').style.backgroundColor = 'white';
+        document.getElementById('content-tivi').style.color = 'black';
+        document.getElementById('currentChanel').style.color = 'black';
+        document.getElementById('currentVolumn').style.color = 'black';
+    }
+
+    turnOff(){
+        this.status = false;
+        document.getElementById('content-tivi').style.backgroundColor = 'darkgrey';
+        document.getElementById('content-tivi').style.color = 'darkgrey';
+        document.getElementById('currentChanel').style.color = 'darkgrey';
+        document.getElementById('currentVolumn').style.color = 'darkgrey';
+    }
+
+    upVolumnTivi(){
+        this.currentVolumn++;
+        displayLGD()
+    }
+
+    downVolumnTivi(){
+        this.currentVolumn--;
+        displayLGD()
+    }
+}
